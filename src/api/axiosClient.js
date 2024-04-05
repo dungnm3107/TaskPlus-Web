1c768bd40c90e0b81f695696ad1aecd7b17727ea
NotnullServer <manhdung1602003@gmail.com>
2024-04-05 20:50:48 +0700

    fix_cors_2

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://taskplus-api.onrender.com'
+const baseUrl = 'https://taskplus-api.onrender.com/api/v1/'
+//http://127.0.0.1:5000/api/v1/
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
